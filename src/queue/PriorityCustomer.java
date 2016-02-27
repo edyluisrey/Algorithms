204dcc93fcd040b8b7fd482eff830fd2d247e371
edyluisrey <eaguirre378@gmail.com>
2016-02-27 16:27:39 -0500

    add test case priorityQueue

--- src/queue/PriorityCustomer.java
+++ src/queue/PriorityCustomer.java
@@ -1,6 +1,8 @@
 package queue;
 
 import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 class Customer {
 	    private int id;
@@ -36,6 +38,26 @@ public class PriorityCustomer{
 		public int compare(Customer one, Customer two) {
 			return two.getId() - one.getId();
 		}
+	}
+	
+	public static void main(String[] args) {
+		 PQsort pqs = new PQsort();
+		 Queue<Customer> PriorityCustomer = new PriorityQueue<>(2,pqs);
+		 Customer  c1= new Customer(4,"Juan");
+		 Customer  c2= new Customer(1,"Manuel");
+		 Customer  c3= new Customer(2,"Edy");
+		 
+		 PriorityCustomer.add(c1);
+		 PriorityCustomer.add(c2);
+		 PriorityCustomer.add(c3);
+		 
+		 Customer ctm1 = PriorityCustomer.poll();
+		 Customer ctm2 = PriorityCustomer.poll();
+		 Customer ctm3 = PriorityCustomer.poll();
+		 System.out.println(ctm1.getId()+ " : "+ ctm1.getName());
+		 System.out.println(ctm2.getId()+ " : "+ ctm2.getName());
+		 System.out.println(ctm3.getId()+ " : "+ ctm3.getName());
+	      
 	}
  
 }
