4be6b6bdd43e4773f96686db9b9a0ebb9a6476fd
edyluisrey <eaguirre378@gmail.com>
2016-03-06 13:25:38 -0500

    add bubble sort algorithm for order objects

--- src/sortSearch/BubbleSort.java
+++ src/sortSearch/BubbleSort.java
@@ -1,5 +1,7 @@
 package sortSearch;
 
+import java.util.Vector;
+
 interface ComparePrices{
 	boolean lessThan(Object o);
     boolean greaterThan(Object o);
@@ -23,11 +25,29 @@ class VegetarianDish implements ComparePrices{
 	@Override
 	public boolean greaterThan(Object o) {
 		// TODO Auto-generated method stub
-		return false;
+		VegetarianDish vd2= (VegetarianDish) o;
+		return price > vd2.price;
 	}
 	
 }
 
 public class BubbleSort {
-
+   
+	public static  void sort(Vector<Object> v){
+	   int pass,j;
+	   boolean flag=true;
+	   for(pass=0;pass< (v.size()-1) && flag; pass++){
+		   flag=false;
+		   for(j=0;j< v.size()-(pass-1);j++){
+			   VegetarianDish vd= (VegetarianDish)v.elementAt(j);
+			   if(vd.greaterThan(v.elementAt(j+1))){
+				   flag=true;
+				   Object temp= v.elementAt(j);
+				   v.setElementAt(v.elementAt(j+1), j);
+				   v.setElementAt(temp, j+1);
+			   }
+					   
+		   }
+	   }
+	}
 }
