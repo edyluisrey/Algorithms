2755ce0efc505c575dae597be713a74b001a602b
edyluisrey <eaguirre378@gmail.com>
2016-03-06 13:45:29 -0500

    add test case order objects

--- src/sortSearch/BubbleSort.java
+++ src/sortSearch/BubbleSort.java
@@ -29,25 +29,48 @@ class VegetarianDish implements ComparePrices{
 		return price > vd2.price;
 	}
 	
+	@Override
+	public String toString(){
+		return "price:"+price+ " name:"+name;
+	}
+	
 }
 
 public class BubbleSort {
    
-	public static  void sort(Vector<Object> v){
-	   int pass,j;
+	public static  void sort(Vector v){
+	   int j;
 	   boolean flag=true;
-	   for(pass=0;pass< (v.size()-1) && flag; pass++){
-		   flag=false;
-		   for(j=0;j< v.size()-(pass-1);j++){
-			   VegetarianDish vd= (VegetarianDish)v.elementAt(j);
-			   if(vd.greaterThan(v.elementAt(j+1))){
-				   flag=true;
-				   Object temp= v.elementAt(j);
-				   v.setElementAt(v.elementAt(j+1), j);
-				   v.setElementAt(temp, j+1);
-			   }
-					   
-		   }
+	   Object temp;
+	   while ( flag )
+	   {
+	       flag= false;    
+	       for( j=0;  j < v.size() -1;  j++ )
+	       {    
+	    	    VegetarianDish vd= (VegetarianDish)v.elementAt(j); 
+	    	    if(vd.greaterThan(v.elementAt(j+1))){
+					   flag=true;
+					   temp= v.elementAt(j);
+					   v.setElementAt(v.elementAt(j+1), j);
+					   v.setElementAt(temp, j+1);
+				   }
+	        }
 	   }
+	}
+	
+	public static void main(String[] args) {
+		VegetarianDish vd1= new VegetarianDish(3,"Lettuce sandwich");
+		VegetarianDish vd2= new VegetarianDish(1,"Broccoli slaw");
+		VegetarianDish vd3= new VegetarianDish(2,"Green papaya salad");
+		Vector<VegetarianDish> v = new Vector<VegetarianDish>();
+		v.add(vd1);
+		v.add(vd2);
+		v.add(vd3);
+		sort(v);
+		for (Object o : v) {
+		    System.out.print(o + " ");
+		}
+		
+	
 	}
 }
