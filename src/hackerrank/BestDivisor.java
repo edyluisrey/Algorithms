0feaf792eca8b2837472a3344b1056f853cdc905
edyluisrey <eaguirre378@gmail.com>
2016-11-30 22:34:15 -0600

    hackerrank contest problems

--- src/hackerrank/BestDivisor.java
+++ src/hackerrank/BestDivisor.java
@@ -9,20 +9,34 @@ public class BestDivisor {
 		Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int max=0,sum=0;
-        
         for(int i=1;i<=n;i++){
-            if(n%i==0){
+            if(n%i==0){   
                 if(i<10){
                     if(max<i)
                         max=i;
-                }else{
-                    sum=i%9;
+                }else{  
+                    sum=total(i);                    
                     if(max<sum)
                         max=i;
                 }
             }
         }
         System.out.println(max);
+	}
+	
+	static int total(int n){
+        int total = 0;
+        while(n > 1)
+        {   
+        	if(n>=10){
+        		total+=n%10;
+                n=n/10; 	
+        	}else{
+        		total+=n;
+        		n=0;
+        	}      
+        }
+        return total;
 	}
 
 }
---
+++ src/hackerrank/Twin.java
@@ -0,0 +1,36 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+
+public class Twin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner in= new Scanner(System.in);
+        int n= in.nextInt();
+        int m= in.nextInt();
+        int count=0;
+        for(int i=n; i<=m;i++){        	
+        	for(int j=i+1;j<=i+2;j++){  
+        		if(!(j<=m)) break;
+        		int ci=0;
+        		for(int p=1;p<=i;p++){
+        			if(i%p==0) ci++;
+        		}
+        		if(ci==2){
+	        		if(Math.abs(j-i)==2 || i!=1){   
+		        		int  cj=0;		        		
+		        		for(int jp=1;jp<=j;jp++){
+		        			if(j%jp==0) cj++;
+		        		}		        		
+		        		if(cj==2)
+		        			count++; 
+	        		}
+        		}
+        	}
+        }
+        System.out.println(count);
+	}
+
+}
