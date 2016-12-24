a83bb53c5cf2b3626650b2be3f11567247361d50
edyluisrey <eaguirre378@gmail.com>
2016-12-24 10:40:21 -0600

    refactor

--- src/hackerrank/DivisibleSumPairs.java
+++ src/hackerrank/DivisibleSumPairs.java
@@ -7,21 +7,21 @@ public class DivisibleSumPairs {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int k = in.nextInt();
-        int a[] = new int[n];
-        int count=0;
-        for(int a_i=0; a_i < n; a_i++){
-            a[a_i] = in.nextInt();
-        }
-         
-        for(int i = 0; i < n; i++){
-            for(int j = i+1; j < n; j++){
-                if((a[i]+a[j])%k == 0)
-                	count++;
-            }
-        }
-        	
+		int n = in.nextInt();
+		int k = in.nextInt();
+		int a[] = new int[n];
+		int count=0;
+		for(int a_i=0; a_i < n; a_i++){
+		    a[a_i] = in.nextInt();
+		}
+		 
+		for(int i = 0; i < n; i++){
+		    for(int j = i+1; j < n; j++){
+		        if((a[i]+a[j])%k == 0)
+		        	count++;
+		    }
+		}
+			
 		System.out.println(count);
 
 	}
