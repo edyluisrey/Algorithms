d0cb05901006dc4418830bb508fde7ad1b313d1c
edyluisrey <eaguirre378@gmail.com>
2018-08-22 23:49:28 -0400

    optimal solution for minimum swaps

--- src/hackerrank/MinimumSwaps2.java
+++ src/hackerrank/MinimumSwaps2.java
@@ -1,25 +1,76 @@
 package hackerrank;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MinimumSwaps2 {
+	
 	// Complete the minimumSwaps function below.
-    static int minimumSwaps(int[] arr) {
-       int n = arr.length;
-       if(n<2) return 0;
-       int re =0; 
-       for(int i =0; i< n; i++){
-           if((arr[i])==i+1) continue;
-           else{
-               int j= i;
-               while(j<n && arr[j]!=(i+1)){
-                   j++; 
-               }
-               int temp=arr[j];
-               arr[j]=arr[i];
-               arr[i]=temp;
-               re++;               
-           }
-       } 
-       return re;
-    }
+	static int minimumSwaps(int[] arr) {
+	   int n = arr.length;
+	   if(n<2) return 0;
+	   int re =0; 
+	   int [] order = Arrays.copyOf(arr, arr.length);
+	   boolean[] visited = new boolean[n]; 
+	   mergeSort(order,0,n-1);
+	   Map<Integer, Integer> map = new HashMap<>();
+	   for(int i =0; i<n; i++){
+	     map.put(order[i], i);   
+	   } 
+	    
+	   for(int i =0; i< n; i++){
+	       if (visited[i] || arr[i] == order[i])
+	            continue;
+	       else{  
+	           int nodes=0;
+	           int j=i;
+	           while(!visited[j]){ 
+	               visited[j]= true;
+	               j= map.get(arr[j]);
+	               nodes++;
+	           }
+	           re+=(nodes-1);
+	       }           
+	   } 
+	   return re;
+	    
+	}
+	
+	static void mergeSort(int[] nums, int l, int p){
+	    if(l==p){
+	        return;
+	    }
+	    int mid = (l+p)/2;
+	    mergeSort(nums, l, mid);
+	    mergeSort(nums, mid+1, p);
+	    merge(nums, l, mid+1, p);
+	}
+	
+	static void merge(int[] nums, int left, int mid, int rightEnd){
+	    int right = mid;  
+	    int k=0;
+	    int n= rightEnd-left+1;
+	    int lower =left;
+	    int[] temp= new int[n]; 
+	    while(left<=mid-1 &&  right<= rightEnd){
+	        if(nums[left]<=nums[right]){
+	            temp[k++]= nums[left++];
+	        }else{
+	            temp[k++]= nums[right++];
+	        }
+	    } 
+	    while(left<=mid-1){
+	        temp[k++]= nums[left++];
+	    }
+	    while(right<=rightEnd){
+	        temp[k++]= nums[right++];
+	    }
+	    
+	    for(int i=0; i<n;i++){
+	        nums[lower+i] = temp[i];
+	    }
+	    
+	}
 
 }
