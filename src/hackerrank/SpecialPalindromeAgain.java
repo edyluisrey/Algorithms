40082e746ab0dc7720c549eeb85cae1953e2fdac
edyluisrey <eaguirre378@gmail.com>
2018-09-18 22:50:30 -0400

    Special Palindrome Again- optimal

--- src/hackerrank/SpecialPalindromeAgain.java
+++ src/hackerrank/SpecialPalindromeAgain.java
@@ -3,23 +3,28 @@ package hackerrank;
 public class SpecialPalindromeAgain {
 	
 	// Complete the substrCount function below.
-	static long substrCount(int n, String s) {
-	    if (n<=1) return n;
-	    int re=0;
-	    for(int i=0; i< (n-1);i++){
-	        for(int j=i+2; j<=n; j++){ 
-	            if(j<n && isPalindrome(s.substring(i,j))) re++;
-	            if(j>=n && isPalindrome(s.substring(i))) re++;
-	        }
-	    }
-	    return n+re;
-	}
-	
-	static boolean isPalindrome(String s){  
-	    char pre = s.charAt(0);
-	    for(int i=0; i< s.length()/2; i++){
-	        if(pre!=s.charAt(i) || pre!=s.charAt((s.length()-1)-i)) return false;
-	    }
-	    return true;
-	}
+    static long substrCount(int n, String s) {
+        if (n<=1) return n;
+        int sum=0;
+        for(int i =0; i< n; i++){
+            char pre = s.charAt(i);
+            int cont = 1;
+            while((i+1)<n && pre==s.charAt(i+1)){
+                i++;
+                cont++;
+            }
+            sum+= (cont*(cont-1))/2;
+        }
+        for(int i=1; i< (n-1); i++){
+            int l=i-1;
+            int r=i+1;  
+            char prev=s.charAt(i+1);            
+            while(r<n && l>=0 && s.charAt(l)==s.charAt(r) && s.charAt(i)!=s.charAt(r) && prev==s.charAt(r)){
+                l--;
+                r++;
+                sum=sum+1; 
+            }
+        }
+        return n+sum;
+    }
 }
